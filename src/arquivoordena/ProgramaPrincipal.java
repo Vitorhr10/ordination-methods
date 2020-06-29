e56a78b032339d5fe6516623861ab93368529b43
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2020-06-29 20:16:32 -0300

    initial commit

---
+++ src/arquivoordena/ProgramaPrincipal.java
@@ -0,0 +1,25 @@
+package arquivoordena;
+
+import java.io.IOException;
+
+public class ProgramaPrincipal {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Digite a palavra: ");
+        Arquivo arquivo = new Arquivo();
+        Ordenacao ordenar = new Ordenacao();
+        int[] arrayDesordenado = new int[65000];
+        int[] arrayOrdenado = new int[65000];
+        
+        arrayDesordenado = arquivo.lerArquivo("C:\\database\\database.txt");
+        
+        //arrayOrdenado = ordenar.insertionSort(arrayDesordenado);
+        //arrayOrdenado = ordenar.selectionSort(arrayDesordenado);
+        //arrayOrdenado = ordenar.bubbleSort(arrayDesordenado);
+        arrayOrdenado = ordenar.mergeSort(arrayDesordenado);
+        arrayOrdenado = ordenar.quickSort(arrayDesordenado);
+        arrayOrdenado = ordenar.heapSort(arrayDesordenado);
+        arquivo.gravarArquivo("C:\\database\\databaseOrdenada.txt", arrayOrdenado);
+    }
+}
